37d4878d5f157e898f0753878262b6a35c6db219
Max Östermann <58210122+maxoest470@users.noreply.github.com>
2023-03-31 11:06:41 +0200

    Publish FibonacciService endpoint in TestWsServer

--- lab1/lab1-example/src/main/java/de/hska/iwi/vislab/lab1/example/TestWsServer.java
+++ lab1/lab1-example/src/main/java/de/hska/iwi/vislab/lab1/example/TestWsServer.java
@@ -4,14 +4,23 @@ import javax.swing.JOptionPane;
 import javax.xml.ws.Endpoint;
 
 import de.hska.iwi.vislab.lab1.example.ws.BuecherServiceImpl;
+import de.hska.iwi.vislab.lab1.example.ws.FibonacciServiceImpl;
 
 
 /** Testserver fuer den Webservice */
 public class TestWsServer {
 	public static void main(final String[] args) {
+        /**
 		String url = (args.length > 0) ? args[0]
 				: "http://localhost:4434/buecherservice";
 		Endpoint ep = Endpoint.publish(url, new BuecherServiceImpl());
+         **/
+
+
+        String url = (args.length > 0) ? args[0]
+                : "http://localhost:4434/fibonacciservice";
+        Endpoint ep = Endpoint.publish(url, new FibonacciServiceImpl());
+
 		JOptionPane.showMessageDialog(null, "TestWsServer beenden");
 		ep.stop();
 	}
